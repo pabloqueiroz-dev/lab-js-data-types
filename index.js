50ef402bacfb0196c39a53dfc238f3b194cbdcba
Pablo Queiroz <pablo.hl2@gmail.com>
2025-11-19 14:02:01 +0000

    Solved lab

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister  = `${s1} ${s2} ${s3} ${s4} ${s5}`
+//const tongueTwister  = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5
 
 // Print out the concatenated string
-
-
+console.log(tongueTwister );
+//console.log(tongueTwister );
 
 
 /*******************************************
@@ -22,10 +23,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const result  = part1.slice(0,-1) + part1.slice(-1).toUpperCase() + part2.slice(0,-1) + part2.slice(-1).toUpperCase() 
 
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -35,10 +36,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = 84 * 0.15
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +48,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 11)
 
 // Print the generated random number
-
+console.log(randomNumber)
 
 
 /*******************************************
@@ -61,16 +62,20 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //false
 
-const expression2 = a || b;
+const expression2 = a || b; //true
 
-const expression3 = !a && b;
+const expression3 = !a && b; //false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //false - corect === *true* (a && b === false  '!' inverts and makes 'true')
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); //true - corect === *false* (a && b === true  '!' inverts and makes 'false')
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+
+console.log(expression1, expression2, expression3, expression4, expression5, expression6, expression7)
+
+//My note: I got both questions wrong, they were the same. Action: Improve understanding of "Logical Operators".
